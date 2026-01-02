acd37fdca1309439574c04297862f13eec55d621
Marissa <marissa.suarman@gmail.com>
2026-01-02 19:03:00 +0700

    Update file

--- Project_2_Funnel_Analysis/sql_files/1_views/01_mv_customer_cohort.sql
+++ Project_2_Funnel_Analysis/sql_files/1_views/01_mv_customer_cohort.sql
@@ -3,6 +3,7 @@
 -- From that first order date, I extract the year to define the cohort year.
 -- This makes it easier to analyze customer behavior based on when they first started buying.
 -- I use a materialized view so the result can be reused without recalculating every time.
+-- Grain: 1 row = 1 customer
 
 CREATE MATERIALIZED VIEW mv_customer_cohort AS
 SELECT
@@ -13,5 +14,6 @@ FROM olist_orders o
 JOIN olist_customers c ON o.customer_id = c.customer_id
 GROUP BY
 	c.customer_unique_id;
+
 
 
